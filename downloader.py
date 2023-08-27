43b0845e627b9f3405c3982591cff97da9bdaa4b
clach04 <clach04@gmail.com>
2023-08-26 22:21:55 -0700

    TODO

--- downloader.py
+++ downloader.py
@@ -9,3 +9,6 @@ urls = json.loads(json_bytes.decode('utf-8'))
 for url in urls:
     if 'simhq.' in url:
         page_content = get_url(url)  # don't do anything with it, just get it cached
+        # TODO if a file type (e.g. gif, png, jpg, jpeg, pdf, etc.) scrape for urls
+        #   1. a href link
+        #   2. img (thumb?)
